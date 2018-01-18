0513186b6b4f51fc436b313a2d36550e8cff0c46
Rimas Mocevicius <rimusz@users.noreply.github.com>
2018-01-18 13:59:27 +0000

    Update build.sh

--- beer-service-web/build.sh
+++ beer-service-web/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=0.1
+TAG=1.0
 
 # exit immediately on failure
 set -e
@@ -9,7 +9,7 @@ set -e
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o beer-service-web-linux
 
 # build docker image
-docker build -t rimusz/beer-service-web:$TAG -t rimusz/beer-service-web:latest .
+docker build -t mesosphere/dcos-beer-service-web:$TAG .
 
 # push to docker registry
-docker push rimusz/beer-service-web
+docker push mesosphere/dcos-beer-service-web
